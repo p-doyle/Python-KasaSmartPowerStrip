91d535a89a02e4c86cd6b4fd2e46a204eed5de32
Pdoyle003 <pdoyle@phigenics.com>
2018-10-07 10:59:16 -0500

    Updated readme Added commands for toggling multiple plugs, realtime/historical energy usage, getting/setting plug name, toggling relay LEDs and rebooting

--- KasaSmartPowerStrip.py
+++ KasaSmartPowerStrip.py
@@ -5,9 +5,10 @@ import struct
 
 class SmartPowerStrip(object):
 
-    def __init__(self, ip, device_id=None, timeout=2.0):
+    def __init__(self, ip, device_id=None, timeout=2.0, protocol='tcp'):
         self.ip = ip
         self.port = 9999
+        self.protocol = protocol
         self.device_id = device_id
         self.sys_info = None
         self.timeout = timeout
@@ -15,23 +16,160 @@ class SmartPowerStrip(object):
         # if the device ID isn't supplied it needs to be queried from
         # the device before any commands can be sent
         if not self.device_id:
-            sys_info = json.loads(self.get_system_info())
+            sys_info = self.get_system_info()
             self.sys_info = sys_info['system']['get_sysinfo']
             self.device_id = self.sys_info['deviceId']
 
+    def set_wifi_credentials(self, ssid, psk, key_type='3'):
+        '''
+        :param ssid: router ssid
+        :param psk: router passkey
+        :param key_type: 3 is WPA2, 2 might be WPA and 1 might be WEP?
+        :return: command response
+        '''
+
+        wifi_command = '{"netif":{"set_stainfo":{"ssid":"' + ssid + '","password":"' + \
+                       psk + '","key_type":' + key_type + '}}}'
+
+        return self.send_command(wifi_command, self.protocol)
+
+    def set_cloud_server_url(self, server_url=''):
+
+        server_command = '{"cnCloud":{"set_server_url":{"server":"' + server_url + '"}}}'
+
+        return self.send_command(server_command, self.protocol)
+
     def get_system_info(self):
+
         return self._udp_send_command('{"system":{"get_sysinfo":{}}}')
 
+    def get_realtime_energy_info(self, plug_num=None, plug_name=None):
+
+        plug_id = self._get_plug_id(plug_num=plug_num, plug_name=plug_name)
+
+        energy_command = '{"context":{"child_ids":["' + plug_id + '"]},"emeter":{"get_realtime":{}}}'
+
+        response = self.send_command(energy_command, self.protocol)
+
+        realtime_energy_data = response['emeter']['get_realtime']
+
+        return realtime_energy_data
+
+    def get_historical_energy_info(self, month, year, plug_num=None, plug_name=None):
+
+        plug_id = self._get_plug_id(plug_num=plug_num, plug_name=plug_name)
+
+        energy_command = '{"context":{"child_ids":["' + plug_id + '"]},' + \
+                         '"emeter":{"get_daystat":{"month": ' + month + ',"year":' + year + '}}}'
+
+        response = self.send_command(energy_command, self.protocol)
+
+        historical_energy_data = response['emeter']['get_daystat']['day_list']
+
+        return historical_energy_data
+
+    def toggle_relay_leds(self, state):
+
+        state_int = self._get_plug_state_int(state, reverse=True)
+
+        led_command = '{"system":{"set_led_off":{"off":' + str(state_int) + '}}}'
+
+        return self.send_command(led_command, self.protocol)
+
+    def set_plug_name(self, plug_num, plug_name):
+
+        plug_id = self._get_plug_id(plug_num=plug_num)
+
+        set_name_command = '{"context":{"child_ids":["' + plug_id + \
+                           '"]},"system":{"set_dev_alias":{"alias":"' + plug_name + '"}}}'
+
+        return self.send_command(set_name_command, self.protocol)
+
+    def get_plug_info(self, plug_num):
+
+        target_plug = [plug for plug in self.sys_info['children'] if plug['id'] == str(int(plug_num)-1).zfill(2)]
+
+        return target_plug
+
+    # toggle multiple plugs by id or name
+    def toggle_plugs(self, state, plug_num_list=None, plug_name_list=None):
+
+        state_int = self._get_plug_state_int(state)
+
+        plug_id_list_str = self._get_plug_id_list_str(plug_num_list=plug_num_list, plug_name_list=plug_name_list)
+
+        all_relay_command = '{"context":{"child_ids":' + plug_id_list_str + '},' + \
+                            '"system":{"set_relay_state":{"state":' + str(state_int) + '}}}'
+
+        return self.send_command(all_relay_command, self.protocol)
+
+    # toggle a single plug
     def toggle_plug(self, state, plug_num=None, plug_name=None):
 
-        plug_id = None
+        state_int = self._get_plug_state_int(state)
+
+        plug_id = self._get_plug_id(plug_num=plug_num, plug_name=plug_name)
+
+        relay_command = '{"context":{"child_ids":["' + plug_id + '"]},' + \
+                    '"system":{"set_relay_state":{"state":' + str(state_int) + '}}}'
+
+        return self.send_command(relay_command, self.protocol)
+
+    def reboot(self, delay=1):
+        reboot_command = '{"system":{"reboot":{"delay":' + str(delay) + '}}}'
+        return self.send_command(reboot_command, self.protocol)
+
+    # manually send a command
+    def send_command(self, command, protocol='tcp'):
+
+        if protocol == 'tcp':
+            return self._tcp_send_command(command)
+        elif protocol == 'udp':
+            return self._udp_send_command(command)
+        else:
+            raise ValueError("Protocol must be 'tcp' or 'udp'")
+
+    def _get_plug_state_int(self, state, reverse=False):
 
         if state.lower() == 'on':
-            state_int = 1
+            if reverse:
+                state_int = 0
+            else:
+                state_int = 1
         elif state.lower() == 'off':
-            state_int = 0
+            if reverse:
+                state_int = 1
+            else:
+                state_int = 0
         else:
-            raise ValueError("invalid state, must be 'on' or 'off'")
+            raise ValueError("Invalid state, must be 'on' or 'off'")
+
+        return state_int
+
+    # create a string with a list of plug_ids that can be inserted directly into a command
+    def _get_plug_id_list_str(self, plug_num_list=None, plug_name_list=None):
+
+        plug_id_list = []
+
+        if plug_num_list:
+            for plug_num in plug_num_list:
+
+                # add as str to remove the leading u
+                plug_id_list.append(str(self._get_plug_id(plug_num=plug_num)))
+
+        elif plug_name_list:
+
+            for plug_name in plug_name_list:
+                # add as str to remove the leading u
+                plug_id_list.append(str(self._get_plug_id(plug_name=plug_name)))
+
+        # convert to double quotes and turn the whole list into a string
+        plug_id_list_str = str(plug_id_list).replace("'", '"')
+
+        return plug_id_list_str
+
+    # get the plug child_id to be used with commands
+    def _get_plug_id(self, plug_num=None, plug_name=None):
 
         if plug_num and self.device_id:
             plug_id = self.device_id + str(plug_num-1).zfill(2)
@@ -39,29 +177,30 @@ class SmartPowerStrip(object):
         elif plug_name and self.sys_info:
             target_plug = [plug for plug in self.sys_info['children'] if plug['alias'] == plug_name]
             if target_plug:
-                plug_id = self.device_id + str(int(target_plug[0]['id']) - 1).zfill(2)
+                plug_id = self.device_id + target_plug[0]['id']
             else:
-                print('Unable to find plug with name ' + plug_name)
+                raise ValueError('Unable to find plug with name ' + plug_name)
+        else:
+            raise ValueError('Unable to find plug.  Provide a valid plug_num or plug_name')
 
-        if plug_id:
-            relay_command = '{"context":{"child_ids":["' + plug_id + '"]},' + \
-                        '"system":{"set_relay_state":{"state":' + str(state_int) + '}}}'
-            return self._tcp_send_command(relay_command)
+        return plug_id
 
     def _tcp_send_command(self, command):
-        print command
+
         sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock_tcp.settimeout(self.timeout)
         sock_tcp.connect((self.ip, self.port))
+
         sock_tcp.send(self._encrypt_command(command))
 
         data = sock_tcp.recv(2048)
         sock_tcp.close()
 
         # the first 4 chars are the length of the command so can be excluded
-        return self._decrypt_command(data[4:])
+        return json.loads(self._decrypt_command(data[4:]))
 
     def _udp_send_command(self, command):
+
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         client_socket.settimeout(self.timeout)
 
@@ -71,10 +210,11 @@ class SmartPowerStrip(object):
 
         data, server = client_socket.recvfrom(1024)
 
-        return self._decrypt_command(data)
+        return json.loads(self._decrypt_command(data))
 
     @staticmethod
     def _encrypt_command(string, prepend_length=True):
+
         key = 171
         result = ''
 
@@ -91,6 +231,7 @@ class SmartPowerStrip(object):
 
     @staticmethod
     def _decrypt_command(string):
+
         key = 171
         result = ''
         for i in string:
--- README.md
+++ README.md
@@ -5,25 +5,97 @@ Amazon link: https://www.amazon.com/Smart-Wi-Fi-Power-Strip-TP-Link/dp/B07G95FFN
 
 Command syntax is fairly similar to the single relay TP Link smart plugs<br/>
 Encrypt/Decrypt code is based on https://github.com/softScheck/tplink-smartplug/blob/master/tplink_smartplug.py<br/>
-The main difference seems to be that the basic get_sysinfo command only works over UDP
+The main difference seems to be that the basic get_sysinfo command only works over UDP, all other commands<br/>
+can use either UDP or TCP
 
-Example code:
+Currently only compatible with Python 2.7 due to differences in how data can be sent via sockets(I think).
+
+## Example code:
 
 ```
 from KasaSmartPowerStrip import SmartPowerStrip
 
 power_strip = SmartPowerStrip('<your power strip ip>')
 
+# get general system info
 print(power_strip.get_system_info())
 
+# get the name and other info of a plug; unless the Kasa app is used the plugs won't have a name by default
+print(power_strip.get_plug_info(1))
+
+# set the name of a plug
+print(power_strip.set_plug_name(1, 'my plug'))
+
+# toggle a plug by number (1-6)
 print(power_strip.toggle_plug('off', plug_num=1))
-print(power_strip.toggle_plug('off', plug_name='Plug 3'))
+
+# toggle a plug by name
+print(power_strip.toggle_plug('on', plug_name='my plug'))
+
+# toggle multiple plugs by number
+print(power_strip.toggle_plugs('on', plug_num_list=[1, 3, 5]))
+
+# toggle multiple plugs by name
+print(power_strip.toggle_plugs('on', plug_name_list=['my plug', 'my plug 2']))
+
+# toggle the leds for each relay on or off
+print(power_strip.toggle_relay_leds('off'))
+
+# get the current energy usage with mA, mV, mW and the total wh
+print(power_strip.get_realtime_energy_info(plug_num=1))
+
+# get a list with the watt hours for each day in the specified month/year
+print(power_strip.get_historical_energy_info(month='10', year='2018', plug_num=1))
+
+# reboot the power strip
+# NOTE this will toggle all relays off/on but will preserve state after rebooting
+# e.g. if it was off before rebooting it will remain off afterwards
+print(power_strip.reboot(5))
 ```
 
-To do:<br>
-Implement functionality to get energy stats.  Can get using:<br>
-```{"context":{"child_ids":["<plug childId>"]},"emeter":{"get_realtime":{}}}```<br>
-  or<br>
-```{"context":{"child_ids":["<plug childId>"]},"emeter":{"get_daystat":{"month":8,"year":2018}}}```
-  
-  
+## Initial Setup
+To setup a new power strip without having to use the Kasa App, which requires you to create a cloud account:
+1. Plug the power strip in and ensure that the status LED is alternating green/orange.  If it isn't press and <br/>
+    hold one of the relay buttons for 5 seconds to perform a factory reset.
+2. Look for and connect to a WiFi network which should start with TP-LINK_Power Strip.<br/>
+    The default IP of the power strip is 192.168.0.1.  It will only accept commands from IP 192.168.0.100, which<br/>
+    it should assign to the first device to connect to its WiFi.
+3. OPTIONAL: If you want to ensure that the power strip never connects to the cloud there are 2 options. <br/>
+    The first is to clear the cloud server URL that is set on the power strip but I can't guarantee that this works. </br>
+    The second is to get the  mac address of the power strip so that you can block outgoing traffic on your <br/>
+    router before allowing it to connect to your network.
+
+```
+power_strip = SmartPowerStrip('192.168.0.1')
+
+print(power_stripset_cloud_server_url(server_url=''))
+
+print(power_strip.get_system_info()['system']['get_sysinfo']['mac'])
+```
+
+4. Use the below code to have it connect to your own WiFi network:
+
+```
+power_strip = SmartPowerStrip('192.168.0.1')
+
+# for WPA2 the key_type is '3', I would guess WPA is '2' and WEP is '1' but I have not tested this
+power_strip.set_wifi_credentials('my ssid', 'my psk', key_type='3')
+```
+
+4. After setting the WiFi info it should restart and then connect to your network at which point<br/>
+    you should be able to begin using it.
+
+<br/><br/>
+## Other commands not yet implemented:
+
+Add countdown timer rules:<br/>
+```{"context":{"child_ids":["<plug childId>"]},"count_down":{"add_rule":{"act":1,"delay":1800,"enable":1,"name":"add timer"}}}```
+
+Get countdown timer rules:<br/>
+```{"context":{"child_ids":["<plug childId>"]},"count_down":{"get_rules":{}}}```
+
+Delete countdown timer rules:<br/>
+```{"context":{"child_ids":["<plug childId>"]},"count_down":{"delete_all_rules":{}}}```
+
+
+
